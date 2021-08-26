6b6035293d11631a48153b48b4b98b47922eb9ce
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-08-26 21:33:57 +0700

    Navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,26 @@
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
 		<div className='my-5'>
 			<p className='text-center text-3xl italic my-5'>Minimal Bitcoin App</p>
 			<div className='flex justify-center text-md space-x-5'>
-				<p>Current price</p>	
+				<p>
+					<Link to='/current'>
+						Current price
+					</Link>
+
+				</p>
 				<p>|</p>
-				<p>Historical price</p>	
+				<p>
+					<Link to='/History/select'>
+						Historical price
+					</Link>
+				</p>
 				<p>|</p>
-				<p>About me</p>	
+				<p>
+					<Link to='/about'> About me</Link>
+				</p>
 			</div>
 		</div>
 	)
