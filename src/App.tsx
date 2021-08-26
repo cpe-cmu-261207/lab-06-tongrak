3e058b707c1f9ba7ed945db2de4df847babace80
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-08-26 21:33:37 +0700

    First commit: current page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,63 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Current from './components/Current';
 
 function App() {
   return (
     <Router>
       <Navbar />
 
-      {/* template for /current */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>Current price</p>
-        <p className='text-2xl'>Loading ...</p>
-        <p className='text-2xl'>{(999999999).toLocaleString()} THB</p>
-        <p> (Last updated) </p>
-      </div>
-
       <br />
 
-      {/* template for /history/select */}
-      <div className='text-center space-y-3 space-x-3'>
-        <p className='text-2xl font-semibold'>Select historical range</p>
-        <span>From date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
-        <span>To date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
-        <br />
-        <button>Get data</button>
-      </div>
+      <Switch>
+        <Route path='/' exact>
+          <Current />
+        </Route>
+        <Route path="/current">
+          <Current />
+        </Route>
+        <Route path="/history/select" exact>
+          {/* template for /history/select */}
+          <div className='text-center space-y-3 space-x-3'>
+            <p className='text-2xl font-semibold'>Select historical range</p>
+            <span>From date</span>
+            <input type='date' onChange={e => console.log(e.target.value)}></input>
+            <span>To date</span>
+            <input type='date' onChange={e => console.log(e.target.value)}></input>
+            <br />
+            <br />
+            <Link to='/history/result' >
+              <button>
+                Get data
+              </button>
+            </Link>
+          </div>
+        </Route>
+        <Route path='/history/result' exact>
+          {/* template for /history/result */}
+          <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>Historical price</p>
+            <p className='text-2xl'>Loading ...</p>
+            <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
+            <p className='text-xl font-semibold'> ( From 2021-01-01 To 2021-01-02)</p>
+            <ul>
+              <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
+              <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
+              <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
+            </ul>
+          </div>
+        </Route>
+        <Route path="/about" exact>
+          {/* template for about me */}
+          <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Tongrak Ruento 630610730</p>
+          </div>
+        </Route>
+      </Switch>
 
-      <br />
-
-      {/* template for /history/result */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>Historical price</p>
-        <p className='text-2xl'>Loading ...</p>
-        <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
-        <p className='text-xl font-semibold'> ( From 2021-01-01 To 2021-01-02)</p>
-        <ul>
-          <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
-        </ul>
-      </div>
-
-      <br />
-
-      {/* template for about me */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>About me</p>
-        <p className='text-xl'>Chayanin Suatap 610631100</p>
-      </div>
-
-    </Router>
+    </Router >
   );
 }
 
---
+++ src/components/Current.tsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { async } from "q";
+import { useState } from "react";
+
+
+
+const Current = () => {
+    const [currPrice, setCurr] = useState<string>();
+    const [updatedDate, setUpdate] = useState<string>();
+    const [loadStatus, setLoadSta] = useState<boolean>(true);
+    const [loadErr, setLoadErr] = useState<boolean>(false);
+
+    const fetchData = async () => {
+        try {
+            const resp = await axios.get('https://api.coindesk.com/v1/bpi/currentprice/thb.json');
+            setCurr(resp.data.bpi.THB.rate)
+            // console.log("currPrice: " + currPrice);
+            setUpdate(resp.data.time.updated)
+            // console.log("currUpdate: " + updatedDate);
+        }
+        catch (err) {
+            setLoadErr(true);
+        }
+        setLoadSta(false);
+    }
+
+    fetchData();
+
+    const render = () => {
+        if(loadStatus)
+            return <p className='text-2xl'>Loading ...</p>
+        else if (loadErr) 
+            return <p className='text-2xl text-red-900'>Loading Fail</p>
+        else {
+            return(
+                <div className='text-center'>
+                    <p className='text-2xl'>{currPrice} THB</p>
+                    <p> (Last updated {updatedDate}) </p>
+                </div>
+            )
+        }
+
+    }
+
+    return (
+        // /* template for /current */
+
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>Current price:</p>
+            {render()}
+        </div>
+    )
+
+}
+
+{/* <p className='text-2xl'>Loading ...</p>
+            <p className='text-2xl'>{(999999999).toLocaleString()} THB</p>
+            <p> (Last updated) </p> */}
+
+export default Current;
